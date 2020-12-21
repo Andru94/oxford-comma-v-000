a9af72c06c7592ea358b8b158f31078cefe3be5b
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-21 07:54:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length() > 2
     array[-1] = 'and ' + array[-1]
   end
+  array
   #array2 = array.shift(array.length()).join(', ')
   #if array.length() > 2
     #array2[]
